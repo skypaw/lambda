0a4c24d2c72cedfffad05f76e646849967ab3614
skypaw <53307746+skypaw@users.noreply.github.com>
2024-04-01 17:58:31 +0200

    Fix string-input

--- lambda-code/main.py
+++ lambda-code/main.py
@@ -10,7 +10,7 @@ def handler(context: argparse.Namespace):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("save json to outputs")
-    parser.add_argument("string-input", type=str, help="String will be written to output")
+    parser.add_argument("--string-input",dest="string-input", type=str, help="String will be written to output")
 
     args = parser.parse_args()
     handler(args)
